6bced65d1d21ba94d2192fae0f0ab8db575ef421
WhiteInfamous24 <juanmanuel.caceres24@outlook.com>
2022-05-22 16:56:29 -0300

    Added some changes

--- Game.java
+++ Game.java
@@ -22,9 +22,40 @@ public class Game {
         board.setPiece(new Bishop(ColorEnum.WHITE), new Position(2, 7));
         board.setPiece(new Bishop(ColorEnum.BLACK), new Position(5, 0));
         board.setPiece(new Bishop(ColorEnum.WHITE), new Position(5, 7));
-        board.setPiece(new Queen(ColorEnum.BLACK), new Position(3, 0));
-        board.setPiece(new King(ColorEnum.WHITE), new Position(3, 7));
-        board.setPiece(new Queen(ColorEnum.WHITE), new Position(4, 7));
-        board.setPiece(new King(ColorEnum.BLACK), new Position(4, 0));
+        board.setPiece(new Queen(ColorEnum.BLACK), new Position(4, 0));
+        board.setPiece(new King(ColorEnum.WHITE), new Position(4, 7));
+        board.setPiece(new Queen(ColorEnum.WHITE), new Position(3, 7));
+        board.setPiece(new King(ColorEnum.BLACK), new Position(3, 0));
+    }
+
+    public void showBoard() {
+        char row = 'A';
+        System.out.println("");
+        for(int i = 0; i < 8; i++) {
+            System.out.println("|---||----------|----------|----------|----------|----------|----------|----------|----------|");
+            System.out.print("| " + row + " |");
+            row++;
+            for(int j = 0; j < 8; j++) { 
+                System.out.print("|");
+                if(board.getPiece(new Position(j, i)) != null)
+                    System.out.printf("%10s", board.getPiece(new Position(j, i)).getNameOfPiece());
+                else
+                    System.out.printf("%10s", board.getPiece(new Position(j, i)));
+            }
+            System.out.println("|");
+            System.out.print("|   |");
+            for(int j = 0; j < 8; j++) {
+                System.out.print("|");
+                if(board.getPiece(new Position(j, i)) != null)
+                    System.out.printf("%10s", board.getPiece(new Position(j, i)).getColorOfPiece());
+                else
+                    System.out.printf("%10s", board.getPiece(new Position(j, i)));
+            }
+            System.out.println("|");
+            System.out.println("|   ||          |          |          |          |          |          |          |          |");
+        }
+        System.out.println("|===||==========|==========|==========|==========|==========|==========|==========|==========|");
+        System.out.println("|   ||1         |2         |3         |4         |5         |6         |7         |8         |");
+        System.out.println("|--------------------------------------------------------------------------------------------|");
     }
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         Game game = new Game();
+        game.showBoard();
+        pause();
         game.initializePieces();
+        game.showBoard();
+    }
+
+    public static void pause() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Press ENTER To Continue...");
+        scanner.nextLine();
+        scanner.close();
     }
 }
